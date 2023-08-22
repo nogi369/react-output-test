df1f730a50c878dc6c881eaeedefa73d0ede8316
nogi <Createc369.Re@gmail.com>
2023-08-22 14:38:58 +0900

    add: onChangeAddInputValueのテストケース, テスト環境の構築

--- src/hooks/useTodo.js
+++ src/hooks/useTodo.js
@@ -1,23 +1,23 @@
-import { INIT_TODO_LIST, INIT_UNIQUE_ID } from "../constants/data"
-import { useState, useMemo } from "react"
+import { INIT_TODO_LIST, INIT_UNIQUE_ID } from "../constants/data";
+import { useState, useMemo } from "react";
 
 export const useTodo = () => {
   // デフォルトTodoList
-  const [ originTodoList, setOriginTodoList ] = useState(INIT_TODO_LIST);
+  const [originTodoList, setOriginTodoList] = useState(INIT_TODO_LIST);
   // 採番ID
-  const [ uniqueId, setUniqueId ] = useState(INIT_UNIQUE_ID);
+  const [uniqueId, setUniqueId] = useState(INIT_UNIQUE_ID);
   // 入力値
-  const [ addInputValue, setAddInputValue ] = useState("");
+  const [addInputValue, setAddInputValue] = useState("");
   // 検索キーワード
-  const [ searchKeyword, setSearchKeyword ] = useState("");
+  const [searchKeyword, setSearchKeyword] = useState("");
   // 表示用TodoList
   const showTodoList = useMemo(() => {
     return originTodoList.filter((todo) => {
       // 修正箇所
-      const regexp = new RegExp("^" + searchKeyword, "i")
-      return todo.title.match(regexp)
-    })
-  },[originTodoList, searchKeyword])
+      const regexp = new RegExp("^" + searchKeyword, "i");
+      return todo.title.match(regexp);
+    });
+  }, [originTodoList, searchKeyword]);
 
   // 入力値の変更処理
   const onChangeAddInputValue = (e) => setAddInputValue(e.target.value);
@@ -28,31 +28,30 @@ export const useTodo = () => {
     if (e.key === "Enter" && addInputValue !== "") {
       // 新規作成するTodoの一意なid
       const nextUniqueId = uniqueId + 1;
-    
+
       // 新規作成するTodoを含めた更新後のTodoList
       const newTodoList = [
         ...originTodoList,
         {
           id: nextUniqueId,
           title: addInputValue,
-        }
-      ]
-      
-      setOriginTodoList(newTodoList)
+        },
+      ];
+
+      setOriginTodoList(newTodoList);
       setUniqueId(nextUniqueId);
       setAddInputValue("");
     }
-  }
+  };
 
   const handleDeleteTodo = (targetId, targetTitle) => {
     // https://fuuno.net/nani/nani02/nani02.html
     if (window.confirm(`「${targetTitle}」のtodoを削除しますか？`)) {
+      const newTodoList = originTodoList.filter((todo) => todo.id !== targetId);
 
-      const newTodoList = originTodoList.filter((todo) => todo.id !== targetId) 
-
-      setOriginTodoList(newTodoList)
+      setOriginTodoList(newTodoList);
     }
-  }
+  };
 
   // 検索処理
   const handleChangeSearchKeyword = (e) => setSearchKeyword(e.target.value);
@@ -60,15 +59,15 @@ export const useTodo = () => {
   const states = {
     showTodoList,
     addInputValue,
-    searchKeyword
+    searchKeyword,
   };
 
   const actions = {
-    onChangeAddInputValue, 
+    onChangeAddInputValue,
     handleAddTodo,
     handleDeleteTodo,
-    handleChangeSearchKeyword
+    handleChangeSearchKeyword,
   };
 
   return [states, actions];
-}
+};
--- src/hooks/useTodo.test.js
+++ src/hooks/useTodo.test.js
@@ -1,17 +1,25 @@
-import { describe, test } from "node:test";
+import { renderHook } from "@testing-library/react";
+import { describe, expect, test } from "vitest";
+import { useTodo } from "./useTodo";
+import { act } from "react-dom/test-utils";
 
 describe("【Hooksテスト】useApp test", () => {
   describe("【関数テスト】onChangeAddInputValue", () => {
     test("【正常系】addInputValueを更新できること", () => {
       // 変数
-      const expectValue = "テスト"
+      const expectValue = "テスト";
       // 引数
       const eventObject = {
         target: {
           value: expectValue,
         },
-      }
-      const { result } = renderHook
-    })
-  })
-})
+      };
+      // hooks呼び出し
+      const { result } = renderHook(() => useTodo());
+      expect(result.current[0].addInputValue).toBe("");
+      // hooks関数の実行
+      act(() => result.current[1].onChangeAddInputValue(eventObject));
+      expect(result.current[0].addInputValue).toBe(expectValue);
+    });
+  });
+});
